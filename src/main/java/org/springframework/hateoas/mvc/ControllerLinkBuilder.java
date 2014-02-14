e392806b0ee57de27b35562c5b6c09d63e5fb996
Manu Whig <manuw@calamp.com>
2014-02-14 14:51:22 -0500

    Now also clearing the uri builder's port if the X-Forwarded-Host header is set. Use any ports from the header and if no ports present in the header, clear out the port set from the current request.

--- src/main/java/org/springframework/hateoas/mvc/ControllerLinkBuilder.java
+++ src/main/java/org/springframework/hateoas/mvc/ControllerLinkBuilder.java
@@ -1,17 +1,14 @@
 /*
  * Copyright 2012-2013 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a
+ * copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations
+ * under the License.
  */
 package org.springframework.hateoas.mvc;
 
@@ -40,176 +37,183 @@ import org.springframework.web.util.UriTemplate;
  * 
  * @author Oliver Gierke
  */
-public class ControllerLinkBuilder extends LinkBuilderSupport<ControllerLinkBuilder> {
+public class ControllerLinkBuilder extends LinkBuilderSupport<ControllerLinkBuilder>
+{
 
-	private static final MappingDiscoverer DISCOVERER = new AnnotationMappingDiscoverer(RequestMapping.class);
-	private static final ControllerLinkBuilderFactory FACTORY = new ControllerLinkBuilderFactory();
+    private static final MappingDiscoverer DISCOVERER = new AnnotationMappingDiscoverer(RequestMapping.class);
+    private static final ControllerLinkBuilderFactory FACTORY = new ControllerLinkBuilderFactory();
 
-	/**
-	 * Creates a new {@link ControllerLinkBuilder} using the given {@link UriComponentsBuilder}.
-	 * 
-	 * @param builder must not be {@literal null}.
-	 */
-	ControllerLinkBuilder(UriComponentsBuilder builder) {
-		super(builder);
-	}
+    /**
+     * Creates a new {@link ControllerLinkBuilder} using the given {@link UriComponentsBuilder}.
+     * 
+     * @param builder must not be {@literal null}.
+     */
+    ControllerLinkBuilder( UriComponentsBuilder builder )
+    {
+        super(builder);
+    }
 
-	/**
-	 * Creates a new {@link ControllerLinkBuilder} with a base of the mapping annotated to the given controller class.
-	 * 
-	 * @param controller the class to discover the annotation on, must not be {@literal null}.
-	 * @return
-	 */
-	public static ControllerLinkBuilder linkTo(Class<?> controller) {
-		return linkTo(controller, new Object[0]);
-	}
+    /**
+     * Creates a new {@link ControllerLinkBuilder} with a base of the mapping annotated to the given controller class.
+     * 
+     * @param controller the class to discover the annotation on, must not be {@literal null}.
+     * @return
+     */
+    public static ControllerLinkBuilder linkTo( Class<?> controller )
+    {
+        return linkTo(controller, new Object[0]);
+    }
 
-	/**
-	 * Creates a new {@link ControllerLinkBuilder} with a base of the mapping annotated to the given controller class. The
-	 * additional parameters are used to fill up potentially available path variables in the class scop request mapping.
-	 * 
-	 * @param controller the class to discover the annotation on, must not be {@literal null}.
-	 * @param parameters additional parameters to bind to the URI template declared in the annotation, must not be
-	 *          {@literal null}.
-	 * @return
-	 */
-	public static ControllerLinkBuilder linkTo(Class<?> controller, Object... parameters) {
+    /**
+     * Creates a new {@link ControllerLinkBuilder} with a base of the mapping annotated to the given controller class. The additional parameters are
+     * used to fill up potentially available path variables in the class scop request mapping.
+     * 
+     * @param controller the class to discover the annotation on, must not be {@literal null}.
+     * @param parameters additional parameters to bind to the URI template declared in the annotation, must not be {@literal null}.
+     * @return
+     */
+    public static ControllerLinkBuilder linkTo( Class<?> controller, Object... parameters )
+    {
 
-		Assert.notNull(controller);
+        Assert.notNull(controller);
 
-		ControllerLinkBuilder builder = new ControllerLinkBuilder(getBuilder());
-		String mapping = DISCOVERER.getMapping(controller);
-		UriTemplate template = new UriTemplate(mapping == null ? "/" : mapping);
+        ControllerLinkBuilder builder = new ControllerLinkBuilder(getBuilder());
+        String mapping = DISCOVERER.getMapping(controller);
+        UriTemplate template = new UriTemplate(mapping == null ? "/" : mapping);
 
-		return builder.slash(template.expand(parameters));
-	}
+        return builder.slash(template.expand(parameters));
+    }
 
-	public static ControllerLinkBuilder linkTo(Method method, Object... parameters) {
+    public static ControllerLinkBuilder linkTo( Method method, Object... parameters )
+    {
 
-		UriTemplate template = new UriTemplate(DISCOVERER.getMapping(method));
-		URI uri = template.expand(parameters);
-		return new ControllerLinkBuilder(getBuilder()).slash(uri);
-	}
+        UriTemplate template = new UriTemplate(DISCOVERER.getMapping(method));
+        URI uri = template.expand(parameters);
+        return new ControllerLinkBuilder(getBuilder()).slash(uri);
+    }
 
-	/**
-	 * Creates a {@link ControllerLinkBuilder} pointing to a controller method. Hand in a dummy method invocation result
-	 * you can create via {@link #methodOn(Class, Object...)} or {@link DummyInvocationUtils#methodOn(Class, Object...)}.
-	 * 
-	 * <pre>
-	 * @RequestMapping("/customers")
-	 * class CustomerController {
-	 * 
-	 *   @RequestMapping("/{id}/addresses")
-	 *   HttpEntity&lt;Addresses&gt; showAddresses(@PathVariable Long id) { … } 
-	 * }
-	 * 
-	 * Link link = linkTo(methodOn(CustomerController.class).showAddresses(2L)).withRel("addresses");
-	 * </pre>
-	 * 
-	 * The resulting {@link Link} instance will point to {@code /customers/2/addresses} and have a rel of
-	 * {@code addresses}. For more details on the method invocation constraints, see
-	 * {@link DummyInvocationUtils#methodOn(Class, Object...)}.
-	 * 
-	 * @param invocationValue
-	 * @return
-	 */
-	public static ControllerLinkBuilder linkTo(Object invocationValue) {
-		return FACTORY.linkTo(invocationValue);
-	}
+    /**
+     * Creates a {@link ControllerLinkBuilder} pointing to a controller method. Hand in a dummy method invocation result you can create via
+     * {@link #methodOn(Class, Object...)} or {@link DummyInvocationUtils#methodOn(Class, Object...)}.
+     * 
+     * <pre>
+     * @RequestMapping("/customers")
+     * class CustomerController {
+     * 
+     *   @RequestMapping("/{id}/addresses")
+     *   HttpEntity&lt;Addresses&gt; showAddresses(@PathVariable Long id) { … } 
+     * }
+     * 
+     * Link link = linkTo(methodOn(CustomerController.class).showAddresses(2L)).withRel("addresses");
+     * </pre>
+     * 
+     * The resulting {@link Link} instance will point to {@code /customers/2/addresses} and have a rel of {@code addresses}. For more details on the
+     * method invocation constraints, see {@link DummyInvocationUtils#methodOn(Class, Object...)}.
+     * 
+     * @param invocationValue
+     * @return
+     */
+    public static ControllerLinkBuilder linkTo( Object invocationValue )
+    {
+        return FACTORY.linkTo(invocationValue);
+    }
 
-	/**
-	 * Wrapper for {@link DummyInvocationUtils#methodOn(Class, Object...)} to be available in case you work with static
-	 * imports of {@link ControllerLinkBuilder}.
-	 * 
-	 * @param controller must not be {@literal null}.
-	 * @param parameters parameters to extend template variables in the type level mapping.
-	 * @return
-	 */
-	public static <T> T methodOn(Class<T> controller, Object... parameters) {
-		return DummyInvocationUtils.methodOn(controller, parameters);
-	}
+    /**
+     * Wrapper for {@link DummyInvocationUtils#methodOn(Class, Object...)} to be available in case you work with static imports of
+     * {@link ControllerLinkBuilder}.
+     * 
+     * @param controller must not be {@literal null}.
+     * @param parameters parameters to extend template variables in the type level mapping.
+     * @return
+     */
+    public static <T> T methodOn( Class<T> controller, Object... parameters )
+    {
+        return DummyInvocationUtils.methodOn(controller, parameters);
+    }
 
-	/* 
-	 * (non-Javadoc)
-	 * @see org.springframework.hateoas.UriComponentsLinkBuilder#getThis()
-	 */
-	@Override
-	protected ControllerLinkBuilder getThis() {
-		return this;
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.springframework.hateoas.UriComponentsLinkBuilder#getThis()
+     */
+    @Override
+    protected ControllerLinkBuilder getThis()
+    {
+        return this;
+    }
 
-	/* 
-	 * (non-Javadoc)
-	 * @see org.springframework.hateoas.UriComponentsLinkBuilder#createNewInstance(org.springframework.web.util.UriComponentsBuilder)
-	 */
-	@Override
-	protected ControllerLinkBuilder createNewInstance(UriComponentsBuilder builder) {
-		return new ControllerLinkBuilder(builder);
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.springframework.hateoas.UriComponentsLinkBuilder#createNewInstance(org.springframework.web.util.UriComponentsBuilder)
+     */
+    @Override
+    protected ControllerLinkBuilder createNewInstance( UriComponentsBuilder builder )
+    {
+        return new ControllerLinkBuilder(builder);
+    }
 
-	/**
-	 * Returns a {@link UriComponentsBuilder} to continue to build the already built URI in a more fine grained way.
-	 * 
-	 * @return
-	 */
-	public UriComponentsBuilder toUriComponentsBuilder() {
-		return UriComponentsBuilder.fromUri(toUri());
-	}
+    /**
+     * Returns a {@link UriComponentsBuilder} to continue to build the already built URI in a more fine grained way.
+     * 
+     * @return
+     */
+    public UriComponentsBuilder toUriComponentsBuilder()
+    {
+        return UriComponentsBuilder.fromUri(toUri());
+    }
 
-	/**
-	 * Returns a {@link UriComponentsBuilder} obtained from the current servlet mapping with the host tweaked in case the
-	 * request contains an {@code X-Forwarded-Host} header and the scheme tweaked in case the request contains an
-	 * {@code X-Forwarded-Ssl} header
-	 * 
-	 * @return
-	 */
-	static UriComponentsBuilder getBuilder() {
+    /**
+     * Returns a {@link UriComponentsBuilder} obtained from the current servlet mapping with the host tweaked in case the request contains an
+     * {@code X-Forwarded-Host} header and the scheme tweaked in case the request contains an {@code X-Forwarded-Ssl} header
+     * 
+     * @return
+     */
+    static UriComponentsBuilder getBuilder()
+    {
 
-		HttpServletRequest request = getCurrentRequest();
-		ServletUriComponentsBuilder builder = ServletUriComponentsBuilder.fromServletMapping(request);
+        HttpServletRequest request = getCurrentRequest();
+        ServletUriComponentsBuilder builder = ServletUriComponentsBuilder.fromServletMapping(request);
 
-		String forwardedSsl = request.getHeader("X-Forwarded-Ssl");
+        String forwardedSsl = request.getHeader("X-Forwarded-Ssl");
 
-		if (StringUtils.hasText(forwardedSsl) && forwardedSsl.equalsIgnoreCase("on")) {
-			builder.scheme("https");
-		}
+        if ( StringUtils.hasText(forwardedSsl) && forwardedSsl.equalsIgnoreCase("on") ) builder.scheme("https");
 
-		String header = request.getHeader("X-Forwarded-Host");
+        String header = request.getHeader("X-Forwarded-Host");
 
-		if (!StringUtils.hasText(header)) {
-			return builder;
-		}
+        if ( !StringUtils.hasText(header) ) return builder;
 
-		String[] hosts = StringUtils.commaDelimitedListToStringArray(header);
-		String hostToUse = hosts[0];
+        String[] hosts = StringUtils.commaDelimitedListToStringArray(header);
+        String hostToUse = hosts[0];
 
-		if (hostToUse.contains(":")) {
+        if ( hostToUse.contains(":") ) {
+            String[] hostAndPort = StringUtils.split(hostToUse, ":");
+            builder.host(hostAndPort[0]);
+            builder.port(Integer.parseInt(hostAndPort[1]));
 
-			String[] hostAndPort = StringUtils.split(hostToUse, ":");
+        }
+        else
+        {
+            builder.host(hostToUse);
+            builder.port(-1);
+        }
 
-			builder.host(hostAndPort[0]);
-			builder.port(Integer.parseInt(hostAndPort[1]));
+        return builder;
+    }
 
-		} else {
-			builder.host(hostToUse);
-		}
-
-		return builder;
-	}
-
-	/**
-	 * Copy of {@link ServletUriComponentsBuilder#getCurrentRequest()} until SPR-10110 gets fixed.
-	 * 
-	 * @return
-	 */
-	@SuppressWarnings("null")
-	private static HttpServletRequest getCurrentRequest() {
-
-		RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
-		Assert.state(requestAttributes != null, "Could not find current request via RequestContextHolder");
-		Assert.isInstanceOf(ServletRequestAttributes.class, requestAttributes);
-		HttpServletRequest servletRequest = ((ServletRequestAttributes) requestAttributes).getRequest();
-		Assert.state(servletRequest != null, "Could not find current HttpServletRequest");
-		return servletRequest;
-	}
+    /**
+     * Copy of {@link ServletUriComponentsBuilder#getCurrentRequest()} until SPR-10110 gets fixed.
+     * 
+     * @return
+     */
+    @SuppressWarnings("null")
+    private static HttpServletRequest getCurrentRequest()
+    {
+        RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
+        Assert.state(requestAttributes != null, "Could not find current request via RequestContextHolder");
+        Assert.isInstanceOf(ServletRequestAttributes.class, requestAttributes);
+        HttpServletRequest servletRequest = ((ServletRequestAttributes)requestAttributes).getRequest();
+        Assert.state(servletRequest != null, "Could not find current HttpServletRequest");
+        return servletRequest;
+    }
 }
